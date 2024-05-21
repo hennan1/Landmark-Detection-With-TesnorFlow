43115c075e93ddeadeac560d1a8bcc3fcf01574d
hennan1 <hannanbutt0267@gmail.com>
2024-05-21 17:32:29 +0500

    Implemented Bitmap.centerCrop

--- app/src/main/java/com/example/landmarkdetection/presentation/BitmapExt.kt
+++ app/src/main/java/com/example/landmarkdetection/presentation/BitmapExt.kt
@@ -5,5 +5,12 @@ import android.graphics.Bitmap
 fun Bitmap.centreCrop(
     desiredWidth:Int,desiredHeight:Int
 ):Bitmap{
+    val xStart = (width - desiredWidth) / 2
+    val yStart = (height - desiredHeight) / 2
 
+    if (xStart < 0 || yStart < 0 || desiredWidth > width || desiredHeight > height){
+        throw IllegalArgumentException("Invalid Arguments for center cropping")
+    }
+
+    return Bitmap.createBitmap(this,xStart,yStart,desiredWidth,desiredHeight)
 }
